b695f729b6bb36c8489ec1bbc21c7aaa7932a961
Tony Spiro <tspiro@tonyspiro.com>
2015-10-28 19:34:35 -0500

    tabs -> spaces

--- pages/Default.jsx
+++ pages/Default.jsx
@@ -11,7 +11,7 @@ import BlogList from '../components/BlogList';
 
 class Home extends React.Component{
 
-	getPage(){
+  getPage(){
 
     let pages = AppStore.data.pages;
 
@@ -19,36 +19,36 @@ class Home extends React.Component{
     let current_slug = this.props.route.path;
     let pages_object = _.indexBy(pages, 'slug');
     let page = pages_object[current_slug];
-	  
+
     // Get page info 
     let metafields = page.metafields;
     let hero = _.findWhere(metafields, { key: 'hero' });
     page.hero = 'https://cosmicjs.com/uploads/' + hero.value;
-    
+
     let headline = _.findWhere(metafields, { key: 'headline' });
     page.headline = headline.value;
 
     let subheadline = _.findWhere(metafields, { key: 'subheadline' });
     page.subheadline = subheadline.value;
     page.main_content = <div dangerouslySetInnerHTML={ {__html: page.content } }></div>;
-  
-	  return page;
 
-	}
-  
+    return page;
+
+  }
+
   render(){
-  	
-  	let globals = AppStore.data.globals;
-  	let pages = AppStore.data.pages;
-  	let page = this.getPage();
 
-  	return (
+    let globals = AppStore.data.globals;
+    let pages = AppStore.data.pages;
+    let page = this.getPage();
+
+    return (
       <div>
         <Header globals={ globals } pages={ pages } page={ page }/>
         <div id="main-content" className="container">
           <div className="row">
             <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-              { page.main_content }
+            { page.main_content }
             </div>
           </div>
         </div>
