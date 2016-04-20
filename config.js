066c313c51ecd29e4f708cb03e801488680b90f6
Tony Spiro <tspiro@tonyspiro.com>
2016-04-20 00:31:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 ================================ */
 let config = {};
 config.bucket = {
-  slug: 'tonyspirocom', // this is your Cosmic JS bucket slug.  Sign up for Cosmic JS (Free) and add a bucket: https://cosmicjs.com/
+  slug: 'tonyspiro-react', // this is your Cosmic JS bucket slug.  Sign up for Cosmic JS (Free) and add a bucket: https://cosmicjs.com/
   read_key: '', // add read_key if added to your Cosmic JS bucket settings
   write_key: '' // add write_key if added to your Cosmic JS bucket settings
 };
